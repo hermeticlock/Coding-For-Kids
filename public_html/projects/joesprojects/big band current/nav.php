4113e990bf41d042c6721fce375dac0c161711da
Joe <rezenbiz@gmail.com>
2017-10-30 16:32:05 -0700

    Brentwood Big Band website being redone - Hero section and Navbar

---
+++ public_html/projects/joesprojects/big band current/nav.php
@@ -0,0 +1,9 @@
+<div>
+<nav class="mynav">
+  <a class="mylogo nava" href="https://www.oddpillars.com/"><span class="glyphicon glyphicon-music"></span> Brentwood Big Band</a>
+  <a href="https://www.oddpillars.com/" class="nava">Home</a>
+  <a href="https://www.oddpillars.com/blog" class="nava">About</a>
+  <a href="https://www.oddpillars.com/tools" class="nava">Performances</a>
+  <a href="#contact" class="nava">Contact</a>
+</nav>
+</div>
